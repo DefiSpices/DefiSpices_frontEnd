f705081d6d8e5159a537d4fe59442a91b2a8873c
Monir-Nassan <mounassan1221@gmail.com>
2022-02-07 23:51:29 +0100

    Transfer tokens function added

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,26 +2,28 @@ import { Card, Row, Col, Input } from "antd"
 import TokenCard from "./TokenCard"
 import TokenContract from "./TokenContractCard"
 import { useState } from "react"
-const {Search} = Input
+const { Search } = Input
 const Cards = (props) => {
     const [createdToken, setCreatedToken] = useState(null)
 
 
-    const handleonSearch = (address)=>{
+    const handleonSearch = (address) => {
         setCreatedToken(address)
 
     }
 
     return (
-        <div style={{  backgroundColor:" #BBD9EB",
+        <div style={{
+            backgroundColor: " #BBD9EB",
 
-            
+
 
             display: "flex",
             flexDirection: "column",
             alignItems: "center",
-            justifyContent: "space-around"}}>
-        
+            justifyContent: "space-around"
+        }}>
+
 
             <Row gutter={50} style={{
                 display: "flex",
@@ -32,7 +34,7 @@ const Cards = (props) => {
 
 
                 <Col span={100} >
-                    <Card title="Create ERC20 Tokens with few Clicks" bordered={false} style={{ background: "#4AE9F9" ,margin: "35px",}} hoverable={true}>
+                    <Card title="Create ERC20 Tokens with few Clicks" bordered={false} style={{ background: "#4AE9F9", margin: "35px", }} hoverable={true}>
                         <TokenCard
                             web3={props.web3}
                             user={props.currentUser}>
@@ -42,7 +44,7 @@ const Cards = (props) => {
                     </Card>
                 </Col>
             </Row>
-            <Search placeholder="Enter You'r Erc20 Token you want to interact with" enterButton="Search" size="middle" loading = {false} onSearch={handleonSearch}  style={{ width: "50%" ,margin: "5px",}}/>
+            <Search placeholder="Enter You'r Erc20 Token you want to interact with" enterButton="Search" size="middle" loading={false} onSearch={handleonSearch} style={{ width: "50%", margin: "5px", }} />
 
             <Row gutter={12} style={{
                 display: "flex",
@@ -51,10 +53,10 @@ const Cards = (props) => {
             }}>
 
                 <Col span={50} >
-                    {createdToken ? 
-                    <Card title="Erc20 Smart contract" bordered={false} >
-                        <TokenContract tokenAddress={createdToken} web3 = {props.web3}></TokenContract>
-                    </Card> : <></>
+                    {createdToken ?
+                        <Card title="Erc20 Smart contract" bordered={false} >
+                            <TokenContract tokenAddress={createdToken} web3={props.web3} currentUser={props.currentUser}></TokenContract>
+                        </Card> : <></>
                     }
                 </Col>
             </Row>
--- src/components/TokenContractCard.js
+++ src/components/TokenContractCard.js
@@ -1,7 +1,8 @@
-import { Col, List, Row, Table, Input, Button } from "antd";
+import { Col, List, Row, Table, Input, Button, Form } from "antd";
+import Title from "antd/lib/typography/Title";
 import { useEffect, useState } from "react";
 import ERC20_Standard from "../contracts/ERC20_Standard.json"
-const {Search} = Input
+const { Search } = Input
 
 
 
@@ -11,14 +12,22 @@ const TokenContract = (props) => {
     const [decimals, setdecimals] = useState();
     const [symbol, setsymbol] = useState();
     const [totalSupply, settotalSupply] = useState();
-    const [ercContract,setErcContract] = useState();
+    const [ercContract, setErcContract] = useState();
     const [balance, setBalance] = useState();
 
-    const getBalance = async (address)=>{
+    const getBalance = async (address) => {
         console.log(address)
         let bal = await ercContract.methods.balanceOf(address).call()
         setBalance(bal)
     }
+
+    const transferTokens = async (values) => {
+        let recipt = await ercContract.methods.transfer(values.address, values.amount).send({ from: (props.currentUser).toString() })
+            .on('receipt', function (receipt) {
+                console.log(receipt)
+            })
+    }
+
     async function loadContract() {
         let ercContract = new props.web3.eth.Contract(ERC20_Standard.abi, (props.tokenAddress).toString())
         let _decimals = await ercContract.methods.decimals().call()
@@ -39,49 +48,84 @@ const TokenContract = (props) => {
 
     return (
         <List>
-        <List.Item>
-            <Col style={{ columngap: '10px' }}>
-                <p>Name</p>
-            </Col>
-            <Col>
-                <p>{name}</p>
-            </Col>
-        </List.Item>
-        <List.Item>
-          <Col style={{ columngap: '10px' }}>
-              <p>Symbol</p>
-          </Col>
-          <Col>
-              <p>{symbol}</p>
-          </Col>
-      </List.Item>
-        <List.Item>
-           <Col style={{ columngap: '10px' }}>
-               <p>Decimals</p>
-           </Col>
-           <Col>
-               <p>{decimals}</p>
-           </Col>
-       </List.Item>
-        <List.Item>
-          <Col style={{ columngap: '10px' }}>
-              <p>totalSupply</p>
-          </Col>
-          <Col>
-              <p>{totalSupply}</p>
-          </Col>
-      </List.Item>
-      <List.Item>
-          <Col style={{ columngap: '10px' }}>
-          <p><Search placeholder="address " enterButton="balanceOf" size="meduim" loading = {false} onSearch={getBalance} /></p>
+            <List.Item>
+                <Col style={{ columngap: '10px' }}>
+                    <p>Name</p>
+                </Col>
+                <Col>
+                    <p>{name}</p>
+                </Col>
+            </List.Item>
+            <List.Item>
+                <Col style={{ columngap: '10px' }}>
+                    <p>Symbol</p>
+                </Col>
+                <Col>
+                    <p>{symbol}</p>
+                </Col>
+            </List.Item>
+            <List.Item>
+                <Col style={{ columngap: '10px' }}>
+                    <p>Decimals</p>
+                </Col>
+                <Col>
+                    <p>{decimals}</p>
+                </Col>
+            </List.Item>
+            <List.Item>
+                <Col style={{ columngap: '10px' }}>
+                    <p>totalSupply</p>
+                </Col>
+                <Col>
+                    <p>{totalSupply}</p>
+                </Col>
+            </List.Item>
+            <List.Item>
+                <Col style={{ columngap: '10px' }}>
+                    <p><Search placeholder="address " enterButton="balanceOf" size="meduim" loading={false} onSearch={getBalance} /></p>
 
-          </Col>
+                </Col>
 
-          <Col>
-          {balance ? <p> {balance} </p> : <></>}
-          </Col>
-      </List.Item>
-      </List>
+                <Col>
+                    {balance ? <p> {balance} </p> : <></>}
+                </Col>
+            </List.Item>
+            <List.Item >
+
+                <Form
+                    name="transfer"
+                    onFinish={transferTokens}
+                    labelCol={{ span: 8 }}
+                    wrapperCol={{ span: 16 }}
+
+                >
+                    <h1 style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center"
+                    }} >Transfer Tokens</h1>
+                    <Input.Group size="large">
+                        <Form.Item label="recipient"
+                            name="address"
+                            rules={[{ required: true, message: 'you need to provide the recipient' }]}
+                        >
+                            <Input />
+                        </Form.Item>
+                        <Form.Item label="amount"
+                            name="amount"
+                            rules={[{ required: true, message: 'you need to provide the amount' }]}
+                        >
+                            <Input />
+                        </Form.Item>
+                    </Input.Group>
+
+                    <Form.Item >
+                        <Button type="danger" htmlType="submit" >Transfer</Button>
+                    </Form.Item>
+                </Form>
+
+            </List.Item>
+        </List>
     )
 
 }
