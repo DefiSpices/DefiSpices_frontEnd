88fa5e6e45c9b9f88e42bc73e991996ab6d34b6d
Monir-Nassan <mounassan1221@gmail.com>
2022-02-07 04:04:05 +0100

    modified the state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import Navbar from './components/Navbar'
 import TokenCard from './components/TokenCard'
-import {  Space, Row, Card , Typography} from 'antd';
+import Cards from './components/Cards'
+import { Space, Row, Card, Typography, Col } from 'antd';
 import { useState } from 'react';
 import Web3 from 'web3';
 const { Title, Paragraph, Text, Link } = Typography;
@@ -10,43 +11,34 @@ function App() {
   const [web3, setWeb3] = useState(null)
   const [currentUser, setCurrentUser] = useState(null)
 
-  const handleProvider = (provider)=>{
+  const handleProvider = (provider) => {
     setWeb3(provider)
   }
 
-  const handleUser = (user)=>{
+  const handleUser = (user) => {
     setCurrentUser(user)
   }
- 
+
 
   return (
     <div >
-    <Navbar setProvider={ (provider)=> handleProvider(provider)}
-             setUser ={ (user)=> handleUser(user)}
-    ></Navbar>
+      <Navbar setProvider={(provider) => handleProvider(provider)}
+        setUser={(user) => handleUser(user)}
+      ></Navbar>
 
-    <div className='Title' style={{background: "#B8F4FA"}}>
+      <div className='Title' style={{ background: "#B8F4FA" }}>
 
-    <Title style={{textAlign: "center"}}>Defi Spices</Title>
-
-    <Paragraph>
+        <Title style={{ textAlign: "center" }}>Defi Spices</Title>
+        <Paragraph>
           Defi Spices is a decentrlized platform that offers Web3 services
-    </Paragraph>
-    </div>
-    <Space direction="horizontal" >
-    {web3 ? 
-    
-      <Card  style={{background: "#4AE9F9", display: 'contents'}}> 
-        <TokenCard 
-                  web3 = {web3}
-                  user = {currentUser}>
-                    
-                  </TokenCard>
-        
-      </Card>
-      
-      : <p>Connect you'r wallet to access the dapp</p>}
-    </Space>  
+        </Paragraph>
+      </div>
+
+      <div className="site-card-wrapper">
+        {web3 ?
+          <Cards web3={web3} currentUser={currentUser} /> : <p>Connect you'r wallet to access the dapp</p>}
+      </div>
+
     </div>
   );
 }
